d360d10f05ac81841583135e5f01ba79656abbd8
Moskalenko Sergey <oknelaksoms@gmail.com>
2018-09-02 13:58:11 +0300

    рисование циклических узоров и движений

---
+++ moskal_10-f(1)_07.05.18/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace moskal_10_f_1__07._05._18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Намалювати сходинки";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(327, 14);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(219, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Рух двох куль";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(555, 14);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Вихід";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(419, 63);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(84, 26);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(264, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Кількість сходинок";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 585);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Ellipses";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ moskal_10-f(1)_07.05.18/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace moskal_10_f_1__07._05._18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //n ступенек
+            int x=75, y=50;//ступеньки будут подниматься на 50 и продливаться на 75
+            this.Refresh();
+            int n = Convert.ToInt32(textBox1.Text), h, w;
+            g = CreateGraphics();
+            Random r = new Random();
+            x0 = 10;
+            y0 = this.Height *2/ 3;
+            for (int i = 0; i < n; i++)
+            {
+                g.DrawLine(Pens.Black, x0, y0, x0, y0 - y);
+                y0 -= y;
+                g.DrawLine(Pens.Black, x0, y0, x0 + x, y0);
+                x0 += x;
+            }
+
+        }
+
+        Graphics g ;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //движение кругов
+            g = CreateGraphics();
+            timer1.Interval = 1;
+            timer2.Interval = 1;
+            x1 = 0;
+            y1 = 0;
+            x2 = 0;
+            y2 = this.Height - 50;
+            timer1.Enabled = !timer1.Enabled;
+            timer2.Enabled = !timer2.Enabled;
+
+        }
+        int x1, y1, x2, y2, x0, y0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.White;
+        }
+
+        //эти два таймера можно запихнуть в один
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //з лівого верхнього кута            
+            Pen p1 = new Pen(Color.Black);
+            Pen p2 = new Pen(Color.White);
+            g.DrawEllipse(p2, x1, y1, 50, 50);            
+            x1++;
+            y1++;
+            g.DrawEllipse(p1, x1, y1, 50, 50);
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //з лівого нижнього кута
+            Pen p1 = new Pen(Color.Black);
+            Pen p2 = new Pen(Color.White);
+            g.DrawEllipse(p2, x2, y2, 50, 50);
+            x2++;
+            y2--;
+            g.DrawEllipse(p1, x2, y2, 50, 50);
+        }
+        
+        
+    }
+}
